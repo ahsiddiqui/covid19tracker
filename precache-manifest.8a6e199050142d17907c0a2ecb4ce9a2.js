b5a5f2e251157d7e1d581caa479cd786995b89f2
Adil H. Siddiqui <ahsiddiqui@modern-electronics.com>
2020-07-28 20:54:46 +0300

    Updates

---
+++ precache-manifest.8a6e199050142d17907c0a2ecb4ce9a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe402edfc0e327f919d42fc357eec59",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "32e3105fdc708c703d6a",
+    "url": "/covid19tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "db14c4b6f5825bd1dc98",
+    "url": "/covid19tracker/static/css/main.5e24b1dc.chunk.css"
+  },
+  {
+    "revision": "32e3105fdc708c703d6a",
+    "url": "/covid19tracker/static/js/2.8cbabdca.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19tracker/static/js/2.8cbabdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db14c4b6f5825bd1dc98",
+    "url": "/covid19tracker/static/js/main.2931a7a3.chunk.js"
+  },
+  {
+    "revision": "2cd6ebb882f01ace96d5",
+    "url": "/covid19tracker/static/js/runtime-main.4eeeee50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
