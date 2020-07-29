331c480787ecfaf72d31bf4a32330a83922d0fff
Adil H. Siddiqui <ahsiddiqui@modern-electronics.com>
2020-07-29 11:11:25 +0300

    covid 19 tracker 1.1

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
+import numeral from "numeral"; 
 
 function InfoBox({ title, subtitle, cases, isRed, active, total, ...props }) {
     return (
@@ -10,10 +11,10 @@ function InfoBox({ title, subtitle, cases, isRed, active, total, ...props }) {
         >
             <CardContent>
                 <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{numeral(cases).format("0,0")}</h2>
                 <Typography className="infoBox__title" color="textSecondary">{subtitle}</Typography>
                 {/* <Typography className="infoBox__total" color="textSecondary">{total}</Typography> */}
-                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{total}</h2>
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{numeral(total).format("0,0")}</h2>
             </CardContent>
         </Card>
     )
